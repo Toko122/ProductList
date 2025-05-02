d3724797f0553089e0870edd8abc5f416c14bd7f
Toko122 <toko.migine@gmail.com>
2025-05-02 23:56:14 +0400

    Update Popup.js

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -5,7 +5,7 @@ const Popup = ({ selectedProducts, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white px-8 py-6 flex flex-col gap-4 justify-start rounded-md relative w-[600px] sm:max-h-none sm:overflow-y-auto max-h-[90vh] overflow-y-auto">
+      <div className="bg-white px-8 py-6 flex flex-col gap-4 justify-start rounded-md relative w-[600px] max-h-[90vh] overflow-y-auto">
         <button onClick={onClose} className="absolute top-2 right-4 text-2xl text-gray-400 hover:text-red-500 font-bold">
           ×
         </button>
@@ -13,7 +13,7 @@ const Popup = ({ selectedProducts, onClose }) => {
         <div className="flex flex-col gap-4 items-start text-center w-full">
           <img src="/assets/images/icon-order-confirmed.svg" alt="checked" className="w-[50px]" />
 
-          <div className="flex flex-col gap-[6px] w-full items-start">
+          <div className="flex flex-col gap-[6px] w-full">
             <span className="sm:text-[38px] text-[28px] font-semibold">Order Confirmed</span>
             <span className="text-[#bbb] text-[14px]">We hope you enjoy your food!</span>
           </div>
@@ -48,7 +48,7 @@ const Popup = ({ selectedProducts, onClose }) => {
             </div>
           </div>
 
-          <button className="w-full py-2 rounded-[20px] sm:px-24 px-14 bg-red-600 flex justify-center items-center sm:text-[16px] text-[14px] text-white font-semibold cursor-pointer hover:bg-red-700 transition duration-400">
+          <button className="w-full py-2 rounded-[20px] sm:px-24 px-14 bg-red-600 flex justify-center items-center sm:text-[16px] text-[14px] text-white font-semibold">
             Start New Order
           </button>
         </div>
